dd856d4f0c51592ef9286a10e1c90f28241d2d0f
Richie Rivera <rtk0211@gmail.com>
2019-07-18 23:10:45 -0500

    Completed check_emai.py
    Completed check_email and created the itineraries class

--- check_email.py
+++ check_email.py
@@ -4,39 +4,7 @@ import smtplib
 import time
 import imaplib
 import email
-
-#%%
-
-def get_full_name(ebody, config, anchor):
-	if config == 'WN':
-		result = ebody[ebody.find('TICKET #'):]
-		result = result[result.find(anchor):]
-		result = result[:result.find('<')]
-		return result[:result.find('\n')-1].split('&nbsp;')
-
-def get_PNR(esubject, airline):
-	if airline == 'WN':
-		return esubject[esubject.find("(")+1:esubject.find(")")]
-
-def get_depart_info(ebody, config):
-	if config == 'WN':
-		info = ebody[ebody.find('DEPARTS'):]
-		info = info[info.find('\r\nong>')+6:]
-		airport_time = info[:info.find('<')]
-		
-		AM_PM = info[info.find(airport_time) + len(airport_time):]
-		AM_PM = AM_PM[AM_PM.find('span>')+len('span>'):AM_PM.find('\r\n')]
-
-		d_date = ebody.replace('\r\n','')
-		d_date = d_date[d_date.find('date')+6:d_date.find('date')+22]
-		d_date = d_date.rstrip().replace('=','').replace(' ','')
-
-		result = airport_time.split(' ')
-		result[1] = result[1] + ' ' + AM_PM
-
-		result.append(d_date)
-		
-		return result
+from itineraries import flight_listing
 
 #%%
 
@@ -49,6 +17,8 @@ smtp_server		= "imap.gmail.com"
 mail = imaplib.IMAP4_SSL(smtp_server)
 mail.login(mail_address,maill_pwd)
 mail.select('inbox')
+
+print('Successfully connected to {} inbox!'.format(mail_address))
 
 #%%
 
@@ -68,7 +38,6 @@ cur_props = pkl.load(open('properties.p','rb'))
 
 if cur_props['inboxcount'] != max(mail_ids): #if we have new emails then we'll process them
 	for email_id in  range(cur_props['inboxcount'], max(mail_ids)):
-		print('\n\nItinerary:')
 		typ, data = mail.fetch(id_list[email_id], '(RFC822)')
 
 		for response in data:
@@ -76,39 +45,18 @@ if cur_props['inboxcount'] != max(mail_ids): #if we have new emails then we'll p
 				msg = email.message_from_string(response[1].decode('utf-8'))
 
 				if 'southwestairlines@ifly.southwest.com' in msg['from']:
-					airline = 'WN'
-					
-					em_sub = msg['subject']
-					em_sub_fn = em_sub[:em_sub.find("'s ")]
+					this_listing = flight_listing('WN', msg)
+					(cur_props['listings'])['WN-' + this_listing.confirmation_number] = this_listing
+				print("Processed listing {} from {} to {} on {}".format(\
+					this_listing.confirmation_number,\
+					this_listing.departure_airport,\
+					this_listing.destination_airport,\
+					this_listing.depart_datetime
+					))
 
-					PNR = get_PNR(em_sub, airline)
-					full_name = get_full_name(msg.get_payload(), airline ,em_sub_fn)
-					depart_info = get_depart_info(msg.get_payload(), airline)
+cur_props['inboxcount'] = max(mail_ids)
 
-					print(full_name)
-					print(PNR)
-					print(depart_info)
-
-
-
-	#pkl.dump(, open('properties.p','wb'))
-
-#%%
-
-# first_email_id = int(id_list[0])
-# latest_email_id = int(id_list[-1])
-
-
-# for i in range(latest_email_id,first_email_id, -1):
-	# typ, data = mail.fetch(i, '(RFC822)' )
-
-	# for response_part in data:
-		# if isinstance(response_part, tuple):
-			# msg = email.message_from_string(response_part[1])
-			# email_subject = msg['subject']
-			# email_from = msg['from']
-			# print 'From : ' + email_from + '\n'
-			# print 'Subject : ' + email_subject + '\n'
+pkl.dump(cur_props, open('properties.p','wb'))
 
 
 #%%
---
+++ itineraries.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+class flight_listing:
+    def __init__(self, carrier, message):
+        if carrier == 'WN':
+            # 1A This section goes through the subject to get the confirmation number
+            t_subject = message['subject'].replace(' (Love) ',' ').replace(' (Hobby) ','')
+            self.confirmation_number = t_subject[t_subject.find("(")+1:t_subject.find(")")]
+
+            # 2A This section goes through the subject to get the passenger's first name
+            self.first_name = message['subject'][:message['subject'].find("'s ")]
+
+            # 3A This section uses the body to determine the passenger's last name
+            t_full_name = message.get_payload()[message.get_payload().find('TICKET #'):]
+            t_full_name = t_full_name[t_full_name.find(self.first_name):]
+            t_full_name = t_full_name[:t_full_name.find('<')]
+            self.last_name = (t_full_name[:t_full_name.find('\n')-1].split('&nbsp;'))[1]
+
+            # 4A This section uses the body to determine the departure airport
+            t_info = message.get_payload()[message.get_payload().find('DEPARTS'):]
+            t_info = t_info[t_info.find('\r\nong>')+6:]
+            t_airport_time = t_info[:t_info.find('<')]
+            t_depart_info = t_airport_time.split(' ')
+            self.departure_airport = t_depart_info[0]
+
+            # 4B This section uses the body and the results from 4A to determine the departure datetime
+            t_AM_PM = t_info[t_info.find(t_airport_time) + len(t_airport_time):]
+            t_AM_PM = t_AM_PM[t_AM_PM.find('span>')+len('span>'):t_AM_PM.find('\r\n')]
+            t_depart_time = t_depart_info[1] + ' ' + t_AM_PM
+            
+            t_depart_date = message.get_payload().replace('\r\n',' ')\
+                .replace('Monday','FunDay!')\
+                .replace('Tuesday','FunDay!')\
+                .replace('Wednesday','FunDay!')\
+                .replace('Thursday','FunDay!')\
+                .replace('Friday','FunDay!')\
+                .replace('Saturday','FunDay!')\
+                .replace('Sunday','FunDay!')
+            t_depart_date = t_depart_date[t_depart_date.find('FunDay!')+9:t_depart_date.find('FunDay!')+19]
+            self.depart_datetime = datetime.strptime(t_depart_date + ' ' + t_depart_time, '%m/%d/%Y %I:%M %p')
+
+            # 5A This section uses the body to determine the destination
+            t_destination = message.get_payload()[message.get_payload().find('ARRIVES'):]
+            self.destination_airport = t_destination[t_destination.find('\r\nng>')+len('\r\nng>'):t_destination.find('\r\nng>')+len('\r\nng>')+3]
