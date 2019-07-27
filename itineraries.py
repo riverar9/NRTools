4d01901cab898352d01154045e90ce512d65bcd8
Richie Rivera <rtk0211@gmail.com>
2019-07-27 00:44:07 -0500

    Fixed Checkin Time Calc
    Went through and re-re-fixed the logic to calculate the datetime that the checkin must be made.

--- itineraries.py
+++ itineraries.py
@@ -52,7 +52,9 @@ class flight_listing:
             t_airport_api_link = "https://airports-api.s3-us-west-2.amazonaws.com/iata/{}.json".format(self.departure_airport.lower())
             t_airport_api_data = requests.get(t_airport_api_link)
             t_dep_tz = pytz.timezone(json.loads(t_airport_api_data.text)['timezone'])
-            self.checkin_datetime = self.depart_datetime.astimezone(get_localzone) - timedelta(days=1)
+            t_min_diff = int(round((datetime.now() - datetime.now(t_dep_tz).replace(tzinfo=None)).total_seconds()/60))
+            t_checkin_datetime = self.depart_datetime + timedelta(minutes=t_min_diff)
+            self.checkin_datetime = t_checkin_datetime - timedelta(hours=24)
 
             # 7A I'm very important so me and mehru are always the first ones to be processed :)
             if self.first_name == 'Richie' and self.last_name == 'Rivera':
